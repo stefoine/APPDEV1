b83a514a5034057ecf5e1a7b17401dae937fd6b5
stefoine <anne.stephanne15@gmail.com>
2026-07-18 14:58:28 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+
+  return (
+    <>
+      <h1>About Me</h1>
+      <hr/>
+      <p>
+        <h2>Hello! My name is Anne Stephanne M. Buenaflor, I am a 3rd year student taking BS in Information Systems.</h2>
+      </p>
+      <p>
+        Fun fact about me:
+      <ul>
+        <li>I love to play video games.</li>
+        <li>I love to watch chinese dramas.</li>
+        <li>I am not a fan of reading books.</li>
+        <li>I love to travel, but have never been outside of the country.</li>
+      </ul>
+      </p>
+      <hr/>
+      <p>
+        Reflection
+      </p>
+      <p>
+        I learned a lot from this course, especially in terms of the tools we had used before. I learned that not all tools are used because they are "widely used", there is a big reason to why it is used. I also learned some of the emerging tools at the moment. For example, I learned about the difference of cloud computing and edge computing. I also learned how to use some of the tools that we had used before, like GitHub and Git. Overall, I learned a lot from this course and I am grateful for the knowledge that I gained from it.
+      </p>
+    </>
+  )
+}
+
+export default App
